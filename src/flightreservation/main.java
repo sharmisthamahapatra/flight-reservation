d2c354e2886e37848a9714138c75ff80aec57971
Gatanugat <gatanugat@Gatanugats-MacBook-Air.local>
2021-06-01 20:52:25 +0530

    checkpoint 5

--- src/flightreservation/main.java
+++ src/flightreservation/main.java
@@ -5,7 +5,22 @@ public class main {
 
     public static void main(String[] args) {
 
-        Flight flight = new Flight();
-        int a = flight.getCapacity();
-    }
+            Ticket regularTicket = new RegularTicket("12345678", "Bhubaneswar",
+                    "Puri", "10 pm", "9 pm",
+                    "100", 1000, false, null, null, "Nothing");
+
+            Ticket touristTicket = new TouristTicket("987654321", "Puri", "Bhubaneswar",
+                    "11 PM", "10 PM",
+                    "13625", 10000, false, null, null,
+                    "Donot know", new String[]{""});
+
+            printTicketDetails(regularTicket);
+            printTicketDetails(touristTicket);
+        }
+
+        public static void printTicketDetails(Ticket ticket)
+        {
+            System.out.println(ticket.getPnr());
+        }
+
 }
